9177ce0d7028f53367036c273ecbc31a120b35ca
Leonardo Aragonez <aragonezleo@gmail.com>
2023-03-30 08:34:54 -0500

    primer commit

---
+++ app/Views/contenido.php
@@ -0,0 +1,5 @@
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1>Hola vista nueva</h1>
+    </main>
---
+++ app/Views/piePagina.php
@@ -0,0 +1,21 @@
+<footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid px-4">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright &copy; punto de venta <?php echo date('y')?></div>
+                    <div>
+                        <a href="#" target="_blank">Facebook</a>
+                        &middot;
+                        <a href="#" target="_blank">Webseite</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+<script src="js/datatables-simple-demo.js"></script>
+</body>
+
+</html>
